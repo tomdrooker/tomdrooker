82686810ccfba707d72fb132e62e6de0af8c4790
Tom Rooker <tomdrooker@gmail.com>
2018-05-26 21:29:07 +0100

    Portfolio implemented

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,34 @@
 $(document).ready(function() {
 
-  $(".project").click(function() {
-    $(".project-info").show();
+  $(".project-one").click(function() {
+    $(".project-info-one").show();
   });
 
-  $(".project").on("mouseleave", function() {
-    $(".project-info").hide();
+  $(".project-one").on("mouseleave", function() {
+    $(".project-info-one").hide();
+  })
+
+  $(".project-two").click(function() {
+    $(".project-info-two").show();
+  });
+
+  $(".project-two").on("mouseleave", function() {
+    $(".project-info-two").hide();
+  })
+
+  $(".project-three").click(function() {
+    $(".project-info-three").show();
+  });
+
+  $(".project-three").on("mouseleave", function() {
+    $(".project-info-three").hide();
+  })
+
+  $(".project-four").click(function() {
+    $(".project-info-four").show();
+  });
+
+  $(".project-four").on("mouseleave", function() {
+    $(".project-info-four").hide();
   })
 });
